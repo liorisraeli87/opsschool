67e90e1aa5741a164d920257e998bb866b2dd3ad
Lior Israeli <liori.israeli87@gmail.com>
2018-08-11 10:46:06 +0300

    Opsschool Excercises

--- vagrant/fixScripts/exercise1-fix.sh
+++ vagrant/fixScripts/exercise1-fix.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-#add fix to exercise1 here
+#install curl because it doesn't come with the image
+sudo apt-get install -y curl
+#remove route definition
+sudo route del www.textfiles.com
--- vagrant/fixScripts/exercise3-fix.sh
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-#add fix to exercise3 here
+#replace deny with allow so the page won't be forbidden
+sudo sed -i '0,/RE/s/deny from all/allow from all/'  /etc/apache2/sites-enabled/000-default
+sudo service apache2 reload
